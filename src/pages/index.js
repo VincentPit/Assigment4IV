79e28fad17f0c38272cafcd7f271281e66b490b1
Junyi Li <jl13122@nyu.edu>
2024-10-25 11:47:42 +0800

    dir chagne

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,15 @@
-// pages/index.js
-import React from 'react';
-import ScatterPlot from './components/scatterPlot'; // Adjust path as necessary
-import * as d3 from 'd3';
+// src/pages/index.js
+import Link from 'next/link';
 
-const data = [
-  { xValue: 10, yValue: 20 },
-  { xValue: 30, yValue: 40 },
-  { xValue: 50, yValue: 60 },
-];
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Site!</h1>
+            <nav>
+                <Link href="/assignment4_Junyi">Assignment 4</Link>
+            </nav>
+        </div>
+    );
+};
 
-const xScale = d3.scaleLinear().domain([0, 100]).range([0, 500]);
-const yScale = d3.scaleLinear().domain([0, 100]).range([500, 0]);
-
-const Home = () => (
-  <svg width={600} height={600}>
-    <ScatterPlot
-      offsetX={50}
-      offsetY={50}
-      data={data}
-      xScale={xScale}
-      yScale={yScale}
-      height={500}
-      width={500}
-    />
-  </svg>
-);
-
-export default Home;
+export default HomePage;
